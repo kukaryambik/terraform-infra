ebbc0ea366a16003b9b151d1e6455e2b0313c2c8
Aleksandr Iakimenko <kukaryambik@gmail.com>
2023-03-29 15:07:37 +0200

    update k8s project

--- do-resources.tf
+++ do-resources.tf
@@ -11,6 +11,8 @@ module "do-k8s-cluster" {
   source  = "kroche-co/k8s-cluster/digitalocean"
   version = "v0.2.2"
 
+  project_name = digitalocean_project.default.name
+
   name                      = "${digitalocean_project.default.name}-${random_pet.do-k8s-cluster.id}"
   region                    = "ams3"
   kubernetes_version_prefix = "1.24."
