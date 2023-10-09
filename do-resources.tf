9622e83bf549ef01db310affece7c7ce73ee9198
ALEKSANDR IAKIMENKO <39311339+kukaryambik@users.noreply.github.com>
2023-10-09 08:34:25 +0000

    feat: update k8s module

--- do-resources.tf
+++ do-resources.tf
@@ -9,7 +9,7 @@ resource "random_pet" "do-k8s-cluster" {}
 
 module "do-k8s-cluster" {
   source  = "kroche-co/k8s-cluster/digitalocean"
-  version = "v0.2.2"
+  version = "v0.2.3"
 
   depends_on = [digitalocean_project.default]
 
