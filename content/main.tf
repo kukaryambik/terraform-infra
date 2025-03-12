3717f55dba557d9885f17b903c061234a9de72a8
Aleksandr Iakimenko <kukaryambik@gmail.com>
2025-03-12 20:41:01 +0200

    fixing

--- content/main.tf
+++ content/main.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "kroche"
+
+    workspaces {
+      name = "kroche-default"
+    }
+  }
+
   required_providers {
     helm = {
       source  = "hashicorp/helm"
