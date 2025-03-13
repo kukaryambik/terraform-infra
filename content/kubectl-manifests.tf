5843d722801d5b38e5cedb6f6a2985e7a63699f3
Aleksandr Iakimenko <kukaryambik@gmail.com>
2025-03-13 15:14:09 +0200

    Changed stringData to data; removed server_side_apply and base64encode

--- content/kubectl-manifests.tf
+++ content/kubectl-manifests.tf
@@ -38,7 +38,6 @@ resource "kubectl_manifest" "onepassword-credentials" {
   depends_on         = [kubectl_manifest.onepassword-connect-ns]
   override_namespace = local.opns
   force_new          = true
-  # server_side_apply  = true
   yaml_body = templatefile(
     "onepassword-connect/secret_onepassword-credentials.yaml",
     { creds = base64encode(var.OP_CREDENTIALS) }
@@ -50,9 +49,8 @@ resource "kubectl_manifest" "onepassword-token" {
   depends_on         = [kubectl_manifest.onepassword-connect-ns]
   override_namespace = local.opns
   force_new          = true
-  # server_side_apply  = true
   yaml_body = templatefile(
     "onepassword-connect/secret_onepassword-token.yaml",
-    { token = base64encode(var.OP_TOKEN) }
+    { token = var.OP_TOKEN }
   )
 }
